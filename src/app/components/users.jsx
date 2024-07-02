cc74c9611ee0f89e9950085acdd56a7e365c6f43
Kostia <kostiakolesnik2513@gmail.com>
2024-07-02 19:23:49 +0300

    add pagination and set type of props

--- src/app/components/users.jsx
+++ src/app/components/users.jsx
@@ -1,32 +1,51 @@
 import React, { useState } from "react";
-import api from "../api";
+import Pagination from "./pagination";
 import User from "./user";
+import paginate from "../utils/paginate";
+import PropTypes from "prop-types";
 
 const Users = ({ users, ...rest }) => {
+  const count = users.length;
+  const pageSize = 4;
+  const [currentPage, setCurrentPage] = useState(1);
+  const handlePageChange = (pageIndex) => {
+    setCurrentPage(pageIndex);
+  };
+  const usersCrop = paginate(users, currentPage, pageSize);
   return (
     <>
-      {users.length > 0 && (
+      {count > 0 && (
         <table className="table">
           <thead>
             <tr>
-              <th scope="col">Имя</th>
-              <th scope="col">Качества</th>
-              <th scope="col">Провфессия</th>
-              <th scope="col">Встретился, раз</th>
-              <th scope="col">Оценка</th>
-              <th scope="col">Избранное</th>
+              <th scope="col">Name</th>
+              <th scope="col">Qualities</th>
+              <th scope="col">Profession</th>
+              <th scope="col">Meetings</th>
+              <th scope="col">Grade</th>
+              <th scope="col">Favorites</th>
               <th />
             </tr>
           </thead>
           <tbody>
-            {users.map((user) => (
+            {usersCrop.map((user) => (
               <User key={user._id} {...rest} {...user} />
             ))}
           </tbody>
         </table>
       )}
+      <Pagination
+        itemsCount={count}
+        pageSize={pageSize}
+        currentPage={currentPage}
+        onPageChange={handlePageChange}
+      />
     </>
   );
+};
+
+Users.propTypes = {
+  users: PropTypes.arrayOf(PropTypes.object).isRequired,
 };
 
 export default Users;
