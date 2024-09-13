b35e0dbd66d6ec891f6c1803d91af4246efc178e
Kostia <kostiakolesnik2513@gmail.com>
2024-09-13 15:30:24 +0200

    created search input & configured searching process

--- src/app/components/usersList.jsx
+++ src/app/components/usersList.jsx
@@ -10,6 +10,7 @@ import _ from "lodash";
 const UsersList = () => {
   const [currentPage, setCurrentPage] = useState(1);
   const [professions, setProfession] = useState();
+  const [searchQuery, setSearchQuery] = useState("");
   const [selectedProf, setSelectedProf] = useState();
   const [sortBy, setSortBy] = useState({ path: "name", order: "asc" });
   const pageSize = 3; // size of showing users on page
@@ -37,10 +38,16 @@ const UsersList = () => {
   }, []);
   useEffect(() => {
     setCurrentPage(1);
-  }, [selectedProf]);
+  }, [selectedProf, searchQuery]);
 
   const handleProfessionSelect = (item) => {
+    if (searchQuery !== "") setSearchQuery("");
     setSelectedProf(item);
+  };
+
+  const handleSearchQuery = ({ target }) => {
+    setSelectedProf(undefined);
+    setSearchQuery(target.value);
   };
 
   //change to selected page
@@ -54,7 +61,12 @@ const UsersList = () => {
   };
 
   if (users) {
-    const filteredUsers = selectedProf
+    const filteredUsers = searchQuery
+      ? users.filter(
+          (user) =>
+            user.name.toLowerCase().indexOf(searchQuery.toLowerCase()) !== -1
+        )
+      : selectedProf
       ? users.filter((user) => {
           return (
             JSON.stringify(user.profession) === JSON.stringify(selectedProf)
@@ -86,6 +98,18 @@ const UsersList = () => {
         )}
         <div className="d-flex flex-column">
           <SearchStatus length={count} />
+          <div class="input-group mb-3">
+            <input
+              type="text"
+              name="searchQuery"
+              className="form-control"
+              placeholder="Search..."
+              aria-describedby="basic-addon1"
+              onChange={handleSearchQuery}
+              value={searchQuery}
+            />
+          </div>
+
           {count > 0 && (
             <UserTable
               users={usersCrop}
